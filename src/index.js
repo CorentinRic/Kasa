f74c9135f933ddcc7284d94a15f730d1f23f0d5d
Corentin <richard.corentin.e@gmail.com>
2022-12-12 13:31:58 +0100

    github - pages

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import Footer from './components/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename='/Kasa'>
     <ScrollToTop />
     <Header />
       <Router />
