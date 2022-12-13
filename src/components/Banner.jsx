45b542e0885afb1e197c27f514928ecf185663bd
Corentin <richard.corentin.e@gmail.com>
2022-12-13 09:50:57 +0100

    Changement méthode / Media queries Hook / Banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,25 +1,16 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import classes from '../styles/components/Banner.module.css'
+import useMediaQuery from '../utils/useMediaQuery'
 
 const Banner = (props) => {
 
     /* Media queries. */
-    const [size, setSize] = useState(window.innerWidth)
-
-    useEffect(() => {
-        const handleResize = () => {
-            setSize(window.innerWidth)
-        }
-        window.addEventListener("resize", handleResize);
-        return () => {
-            window.removeEventListener("resize", handleResize)
-        }
-    },[])
+    const matches = useMediaQuery("(min-width: 600px)");
 
     return (
             <div className={`${classes.banner} ${props.className}`}>
                 {/* Changement de contenu <img /> si la page est supérieur à 600px. */}
-                {size < 600 ? <img className={classes.banner__img} src={props.imageMobile} alt={props.alt} /> : <img className={classes.banner__img} src={props.image} alt={props.alt} />}
+                {matches ? <img className={classes.banner__img} src={props.image} alt={props.alt} /> : <img className={classes.banner__img} src={props.imageMobile} alt={props.alt} />}
                 <h1 className={classes.banner__h1}>{props.title}</h1>
             </div>
     )
